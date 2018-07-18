d2cf348ebe26b334538af9d1264da5cc38cc730d
Oussama Barhoumi <oussama@travelcar.com>
2018-07-18 11:25:56 +0200

    Minor fixes

--- test/suggesters-test.js
+++ test/suggesters-test.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose')
 const elasticsearch = require('elasticsearch')
 const esClient = new elasticsearch.Client({
+  host: 'localhost:9200',
   deadTimeout: 0,
   keepAlive: false
 })
